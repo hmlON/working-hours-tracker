f7f817e6a3b1c18f27aa2275c80732ef4c349c92
Nikita Kholin <nik.kholin@gmail.com>
2017-05-05 22:31:42 +0300

    Set up registration and login on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
+
   def index
+  end
+
+  # this is for devise to work on the homepage
+  helper_method :resource_name, :resource_class, :resource, :devise_mapping
+
+  def resource_name
+    :user
+  end
+
+  def resource_class
+     User
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
   end
 end
