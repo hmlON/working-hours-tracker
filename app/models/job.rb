9a561f8fa45ddd957fb45fae65ff1e986af55a9b
Nikita Kholin <nik.kholin@gmail.com>
2017-05-06 00:15:04 +0300

    Create Job#hours_left_this_week

--- app/models/job.rb
+++ app/models/job.rb
@@ -7,4 +7,12 @@ class Job < ApplicationRecord
   def hours_per_day
     hours_per_week / WORKED_DAYS_COUNT
   end
+
+  def hours_this_week
+    worked_days.this_week.pluck(:hours).sum
+  end
+
+  def hours_left_this_week
+    hours_per_week - hours_this_week
+  end
 end
--- app/models/worked_day.rb
+++ app/models/worked_day.rb
@@ -1,3 +1,5 @@
 class WorkedDay < ApplicationRecord
   belongs_to :job
+
+  scope :this_week, -> { where(date: Date.today.beginning_of_week..Date.today) }
 end
