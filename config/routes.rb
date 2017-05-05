f6c336d3d62f12e7bc750478239407f71fa706af
Nikita Kholin <nik.kholin@gmail.com>
2017-05-05 23:06:05 +0300

    Create dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :jobs, only: [:new, :create]
+
   devise_for :users
+
+  resources :jobs, only: [:new, :create]
+  get 'dashboard' => 'dashboard#index'
 end
