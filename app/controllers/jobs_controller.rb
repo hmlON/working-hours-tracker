e73301b60d74b1d1e42b1a15a0289f59b70a8da7
Nikita Kholin <nik.kholin@gmail.com>
2017-05-09 11:53:53 +0300

    Add job editing ability

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_job, only: [:edit, :update]
 
   def new
     @job = Job.new
@@ -12,6 +13,22 @@ class JobsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit; end
+
+  def update
+    if @job.update(job_params)
+      redirect_to dashboard_path, notice: "Job updated successfuly"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_job
+    @job = current_user.job
   end
 
   def job_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :jobs, only: [:new, :create]
+  resource :job, only: [:new, :create, :edit, :update]
   resources :worked_days, only: [:create]
   get 'dashboard' => 'dashboard#index'
 end
