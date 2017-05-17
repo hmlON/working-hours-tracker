e8d63409d4133ea73da8f82ea811e8949a2ec4ee
Nikita Kholin <nik.kholin@gmail.com>
2017-05-17 10:04:03 +0300

    Add this week to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @worked_days_this_week = current_user.job.worked_days.this_week
     @worked_day = WorkedDay.new(hours: current_user.job.hours_per_day.to_i, date: Date.today)
   end
 end
--- app/models/worked_day.rb
+++ app/models/worked_day.rb
@@ -2,4 +2,8 @@ class WorkedDay < ApplicationRecord
   belongs_to :job
 
   scope :this_week, -> { where(date: Date.today.beginning_of_week..Date.today) }
+
+  def week_day
+    date.strftime('%a')
+  end
 end
