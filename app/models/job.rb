7264d7dc2c99aea97163e99bcfce5d0eba053380
Nikita Kholin <nik.kholin@gmail.com>
2017-05-17 11:06:34 +0300

    Add this week to dashboard

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,10 +2,25 @@ class Job < ApplicationRecord
   belongs_to :user
   has_many :worked_days
 
-  WORKED_DAYS_COUNT = 5
+
+  #TODO: improve this to dynamic job week days
+  def worked_week_days
+    [1, 2, 3, 4, 5].map do |week_day_number|
+      (Date.today.beginning_of_week..Date.today.end_of_week).to_a[week_day_number - 1]
+    end
+  end
+
+  def worked_days_this_week
+    days = worked_days.where(date: Date.today.beginning_of_week..Date.today)
+    this_week = []
+    worked_week_days.each do |date|
+      this_week << (days.find { |day| day.date == date } || NotWorkedDay.new(date))
+    end
+    this_week
+  end
 
   def hours_per_day
-    hours_per_week / WORKED_DAYS_COUNT
+    hours_per_week / worked_week_days.count
   end
 
   def hours_this_week
