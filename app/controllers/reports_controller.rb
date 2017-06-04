5d791c18fd3878832a7b15c5ff40c9037f3a30c7
Nikita Kholin <nik.kholin@gmail.com>
2017-06-04 11:15:33 +0300

    Revert report week ordering

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   def index
-    sundays = current_user.job.created_at.to_date.upto(Date.today).select(&:sunday?)
+    sundays = Date.today.downto(current_user.job.created_at.to_date).select(&:sunday?)
     @weeks = sundays.map { |sunday| WorkedWeek.new(job: current_user.job, date: sunday) }
   end
 end
