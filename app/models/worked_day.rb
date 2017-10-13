19c82400e3c9d488058df727addffcebca2d0575
Nikita Kholin <nik.kholin@gmail.com>
2017-10-14 00:42:05 +0300

    Add today and form styling

--- app/models/worked_day.rb
+++ app/models/worked_day.rb
@@ -1,9 +1,19 @@
 class WorkedDay < ApplicationRecord
   belongs_to :job
 
+  validates :hours, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 24 }
+
   scope :this_week, -> { where(date: Date.today.beginning_of_week..Date.today) }
 
   def week_day
     date.strftime('%a')
+  end
+
+  def today?
+    date == Date.current
+  end
+
+  def today_css_class
+    "today" if today?
   end
 end
